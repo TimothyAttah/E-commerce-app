2959e0c85c4956014e2dd3275a25be22745cf108
Timothy Attah <timothyattah733@gmail.com>
2021-10-23 15:19:05 +0100

    Created slider

---
+++ client/src/components/helper/helper.js
@@ -0,0 +1,81 @@
+import { images } from '../images';
+
+export const sliderItems = [
+  {
+    _id: 1,
+    img: `${ images.Img3 }`,
+    title: 'summer sale',
+    desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+    bg: 'f5fafd'
+  },
+  {
+    _id: 2,
+    img: `${ images.Photo4 }`,
+    title: 'autumn collection',
+    desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+    bg: 'fcf1ed'
+  },
+  {
+    _id: 3,
+    img: `${ images.Img5 }`,
+    title: 'Lounge wear love',
+    desc: "Don't compromise on style! Get flat 30% off for new arrivals.",
+    bg: 'fbf0f4'
+  },
+];
+
+export const categories = [
+  {
+    _id: 1,
+    img: `${images.Img5}`,
+    title: 'shirt style',
+    category: 'women',
+  },
+  {
+    _id: 2,
+    img: `${images.Photo26}`,
+    title: 'loungewear love',
+    category: 'coats',
+  },
+  {
+    _id: 3,
+    img: `${images.Photo27}`,
+    title: 'light jackets',
+    category: 'jeans',
+  },
+];
+
+export const popularProducts = [
+  {
+    _id: 1,
+    img: `${ images.Photo5 }`,
+  },
+  {
+    _id: 2,
+    img: `${ images.Photo6 }`,
+  },
+  {
+    _id: 3,
+    img: `${ images.Photo20 }`,
+  },
+  {
+    _id: 4,
+    img: `${ images.Photo21 }`,
+  },
+  {
+    _id: 5,
+    img: `${ images.Photo22 }`,
+  },
+  {
+    _id: 6,
+    img: `${ images.Photo23 }`,
+  },
+  {
+    _id: 7,
+    img: `${ images.Photo24 }`,
+  },
+  {
+    _id: 8,
+    img: `${ images.Photo25 }`,
+  },
+];
---
+++ client/src/components/slider/Slider.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { Icon } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { mobile } from '../../responsive';
+
+import { sliderItems } from '../helper/helper';
+
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	position: relative;
+	overflow: hidden;
+	${mobile({
+    display: 'none',
+  })}
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${props => props.dir === 'left' && '10px'};
+  right: ${props => props.dir === 'right' && '10px'};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh; 
+  display: flex;
+  align-items: center;
+  background-color: #${props => props.bg}
+`;
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  text-transform: uppercase;
+  font-size: 5rem;
+`;
+const Desc = styled.p`
+  text-transform: uppercase;
+  margin: 50px 0px;
+  font-size: 1.3rem;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  text-transform: uppercase;
+  padding: 10px;
+  font-size: 1.2rem;
+  background: transparent;
+  border: 1px solid gray;
+`;
+
+export const Slider = () => {
+  const [ slideIndex, setSlideIndex ] = useState( 0 );
+  const handleClick = (direction) => {
+    if ( direction === 'left' ) {
+      setSlideIndex( slideIndex > 0 ? slideIndex - 1 : 2 );
+    } else {
+      setSlideIndex( slideIndex < 2 ? slideIndex + 1 : 0 );
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow dir="left" onClick={() => handleClick('left')}>
+        <Icon name="arrow left" />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        { sliderItems.map( sliderItem => (
+          <Slide bg={sliderItem.bg} key={sliderItem.id}>
+            <ImageContainer>
+              <Image src={sliderItem.img} alt="" />
+            </ImageContainer>
+            <InfoContainer>
+              <Title> {sliderItem.title}</Title>
+              <Desc>
+                {sliderItem.desc}
+              </Desc>
+              <Button>show now</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow dir="right" onClick={() => handleClick('right')}>
+        <Icon name="arrow right" />
+      </Arrow>
+    </Container>
+  );
+};

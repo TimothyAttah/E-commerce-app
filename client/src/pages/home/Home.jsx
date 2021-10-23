8e37ba97bab9e2116a6ca401f9faf3ab3f7fb15e
Timothy Attah <timothyattah733@gmail.com>
2021-10-23 12:27:30 +0100

    Created announcement components

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Announcement } from '../../components/Announcement'
 import { Navbar } from '../../components/nav/Navbar'
 
 export const Home = () => {
   return (
     <div>
+      <Announcement />
       <Navbar />
       <h2>Home page</h2>
     </div>

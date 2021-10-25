6605702bc9c0773de4388f60e7b3fb753c3c69b4
Timothy Attah <timothyattah733@gmail.com>
2021-10-25 06:50:30 +0100

    Added register styles

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,57 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { images } from '../../components/images';
-import { mobile } from '../../responsive';
+import {
+  Container,
+  Wrapper,
+  Title,
+  Form,
+  Input,
+  Agreement,
+  Button
+} from './RegisterStyles';
 
-const Container = styled.div`
-	width: 100vw;
-	height: 100vh;
-	background: linear-gradient(
-			rgba(255, 255, 255, 0.5),
-			rgba(255, 255, 255, 0.5)
-		),
-		url(${images.Photo3}) center no-repeat;
-    background-size: cover;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-const Wrapper = styled.div`
-	width: 50%;
-	padding: 20px;
-	background-color: white;
-	${mobile({
-    width: '75%',
-  })}
-`;
-const Title = styled.h1`
-  text-transform: uppercase;
-  font-size: 1.5em;
-  font-weight: 300;
-`;
-const Form = styled.form`
-  display: flex;
-    flex-wrap: wrap;
-`;
-const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 20px 10px 0px 0px;
-  padding: 10px;
-  border: 1px solid gray;
-`;
-const Agreement = styled.span`
-  font-size: 0.7em;
-  margin: 20px 0;
-`;
-const Button = styled.button`
-  text-transform: uppercase;
-  width: 40%;
-  padding: 15px 20px;
-  background-color: teal;
-  color: white;
-`;
 
 export const Register = () => {
   return (

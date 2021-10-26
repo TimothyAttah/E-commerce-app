72dd30f93ff60e2be41650d375cd8179c2352387
Timothy Attah <timothyattah733@gmail.com>
2021-10-26 10:06:27 +0100

    Setup backend and mongo db

--- config/db.js
+++ config/db.js
@@ -1,61 +1,17 @@
-import mongoose from 'mongoose';
-import multer from 'multer';
-import GridFsStorage from 'multer-gridfs-storage';
-import Grid from 'gridfs-stream';
-import path from 'path';
+const mongoose = require('mongoose');
 
-Grid.mongo = mongoose.mongo;
+const connectDB = async () => {
+	try {
+		const mongoDB = await mongoose.connect(process.env.MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			// useCreateIndex: true,
+			// useFindAndModify: false,
+		});
+		console.log(`MongoDB connected on host: ${mongoDB.connection.host}`);
+	} catch (error) {
+		console.log(`Error: ${error}`);
+	}
+};
 
-const mongoURI = 'mongodb://localhost:27017/facebookClone';
-
-export const conn = mongoose.createConnection(mongoURI, {
-	useCreateIndex: true,
-	useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-} );
-
-let gfs;
-
-conn.once( 'open', () => {
-  console.log( 'DB Connected' );
-  gfs = Grid( conn.db, mongoose.mongo )
-  gfs.collection('images')
-} )
-
-const storage = new GridFsStorage( {
-  url: mongoURI,
-  file: ( req, file ) => {
-    return new Promise( ( resolve, reject ) => {
-      const filename = `image-${ Date.now() }${ path.extname( file.originalname ) }`
-      const fileInfo = {
-        filename: filename,
-        bucketName: 'images'
-      };
-      resolve( fileInfo );
-    } );
-  }
-} );
-
-export const upload = multer( { storage } );
-
-mongoose.connect(mongoURI, {
-	useCreateIndex: true,
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-// export const connectDB = async () => {
-//   try {
-//     const mongoDB = await mongoose.connect(process.env.MONGODB__URL, {
-// 			useNewUrlParser: true,
-// 			useCreateIndex: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify:true
-// 		});
-//     console.log(`MongoDB connected on ${mongoDB.connection.host}`);
-//   } catch (err) {
-//     console.log(err);
-//   } 
-// }
-
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,115 +1,32 @@
-import dotenv from 'dotenv';
-dotenv.config( { path: './config/.env' } );
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import multer from 'multer';
-import GridFsStorage from 'multer-gridfs-storage';
-import Grid from 'gridfs-stream';
-import path from 'path';
-import Pusher from 'pusher';
-// import { upload } from './config/db.js';
-import Post from './models/PostModel.js';
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/.env' });
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const multer = require('multer');
 
-// Grid.mongo = mongoose.mongo
-
-
-
-// APP CONFIG
 const app = express();
-const PORT = process.env.PORT || 9000;
+const connectDB = require('./config/db');
 
-// MIDDLEWARES
-app.use( express.json() );
-app.use( cors() );
+connectDB();
 
-// DB CONFIG
-Grid.mongo = mongoose.mongo;
+// app.use('/api/auth', require('./routes/authRoute'));
+// app.use('/api/users', require('./routes/usersRoute'));
+// app.use('/api/products', require('./routes/productRoute'));
+// app.use('/api/carts', require('./routes/cartRoute'));
+// app.use('/api/orders', require('./routes/orderRoute'));
+// app.use('/api/checkout', require('./routes/stripeRoute'));
 
-const mongoURI = 'mongodb://localhost:27017/facebookClone';
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 
-export const conn = mongoose.createConnection(mongoURI, {
-	useCreateIndex: true,
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-});
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
-let gfs;
+const PORT = process.env.PORT || 8080;
 
-conn.once('open', () => {
-	console.log('DB Connected');
-	gfs = Grid(conn.db, mongoose.mongo);
-	gfs.collection('images');
-});
-
-const storage = new GridFsStorage({
-	url: mongoURI,
-	file: (req, file) => {
-		return new Promise((resolve, reject) => {
-			const filename = `image-${Date.now()}${path.extname(file.originalname)}`;
-			const fileInfo = {
-				filename: filename,
-				bucketName: 'images',
-			};
-			resolve(fileInfo);
-		});
-	},
-});
-
-export const upload = multer({ storage });
-
-mongoose.connect(mongoURI, {
-	useCreateIndex: true,
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-// API ROUTES
-app.get( '/', ( req, res ) => res.status( 200 ).send( 'hello world' ) )
-app.post( '/upload/image', upload.single( 'file' ), ( req, res ) => {
-	res.status(201).send(req.file)
-} )
-
-app.post( '/upload/post', ( req, res ) => {
-	const postData = req.body;
-	console.log( postData );
-	Post.create( postData, ( err, data ) => {
-		if ( err ) {
-			res.status(500).send(err)
-		} else {
-			res.status(201).send(data)
-		}
-	})
-})
-app.get( '/retrieve/posts', ( req, res ) => {
-	Post.find( ( err, data ) => {
-		if ( err ) {
-			res.status(500).send(err)
-		} else {
-			data.sort( ( b, a ) => {
-				return a.timestamp - b.timestamp;
-			})
-			res.status(200).send(data)
-		}
-	})
-} )
-
-app.get( '/retrieve/images/single', ( req, res ) => {
-	gfs.files.findOne( { filename: req.query.name }, ( err, file ) => {
-		if ( err ) {
-			res.status( 500 ).send( err );
-		} else {
-			if ( !file || file.length === 0 ) {
-				res.status(404).json({err: 'file not found'})
-			} else {
-				const readstream = gfs.createReadStream( file.filename );
-				readstream.pipe( res );
-			}
-		}
-	})
-})
-
-
-// LISTEN
-app.listen( PORT, () => console.log( `Server started and listening on localhost: ${ PORT }` ) );
+app.listen(PORT, () =>
+	console.log(`Backend server is running on port: ${PORT}`)
+);

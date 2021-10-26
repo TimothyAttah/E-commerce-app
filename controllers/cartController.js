f9eb6044eebf973a1b142ea70a6537e04ff6bd6b
Timothy Attah <timothyattah733@gmail.com>
2021-10-26 13:43:35 +0100

    Update cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -8,9 +8,10 @@ const cartControllers = {
 			const savedCart = await newCart.save();
 			res.status(200).json({ message: 'New Cart added', savedCart });
 		} catch (err) {
-			return res.status(500).json({ error: err });
+			return res.status(500).json({ error: err.message });
 		}
 	},
+
 	// UPDATE
 	updateCart: async (req, res) => {
 		try {
@@ -25,36 +26,39 @@ const cartControllers = {
 				.status(200)
 				.json({ message: 'Cart updated successfully.', updatedCart });
 		} catch (err) {
-			return res.status(500).json({ error: err });
+			return res.status(500).json({ error: err.message });
 		}
 	},
+
 	// DELETE
 	deleteCart: async (req, res) => {
 		try {
 			await Cart.findByIdAndDelete(req.params.id);
 			res.status(200).json('Cart has been deleted... ');
 		} catch (err) {
-			return res.status(500).json({ error: err });
+			return res.status(500).json({ error: err.message });
 		}
 	},
-	// GET CART
+
+	// GET USER CART
 	getUserCart: async (req, res) => {
 		try {
 			const cart = await Cart.findOne({id: req.params.id});
 			res
 				.status(200)
-				.json({ message: 'Cart has been deleted... ', cart });
+				.json(cart);
 		} catch (err) {
-			return res.status(500).json({ error: err });
+			return res.status(500).json({ error: err.message });
 		}
 	},
+
 	// GET ALL CART
 	getAllCarts: async (req, res) => {
 		try {
 			const carts = await Cart.find();
 			res.status(200).json(carts);
 		} catch (err) {
-			return res.status(500).json({ error: err });
+			return res.status(500).json({ error: err.message });
 		}
 	},
 };

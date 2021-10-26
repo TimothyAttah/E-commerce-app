3641fed07d3d650867358f439f0dd384df47fa0a
Timothy Attah <timothyattah733@gmail.com>
2021-10-26 10:26:56 +0100

    Created users routes

---
+++ routes/usersRoute.js
@@ -0,0 +1,22 @@
+const router = require( 'express' ).Router();
+const userControllers = require( '../controllers/usersControllers' );
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require( '../middlewares/auth' );
+
+router.put( '/update/:id', verifyTokenAndAuthorization, userControllers.updateUser );
+
+router.delete( '/delete/:id', verifyTokenAndAuthorization, userControllers.deleteUser );
+
+router.get( '/find/:id', verifyTokenAndAdmin, userControllers.getUser );
+
+router.get( '/', verifyTokenAndAdmin, userControllers.getUsers );
+
+router.get( '/stats', verifyTokenAndAdmin,  userControllers.getUserStats );
+
+router.put( '/:id/follow', userControllers.followUsers );
+
+router.put( '/:id/unfollow', userControllers.unfollowUsers );
+
+router.get( '/friends/:userId', userControllers.getFriends );
+
+
+module.exports = router;

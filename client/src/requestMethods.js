138689bcb9bfe34e9cfd895bfa4cefd0266537a1
Timothy Attah <timothyattah733@gmail.com>
2021-10-27 09:06:02 +0100

    Added token to request api

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api';
-const TOKEN = '';
+const TOKEN =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzdlM2JjM2EzZGU1NzEyNDliYmY0NyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNTMwNzczNCwiZXhwIjoxNjM1NTY2OTM0fQ.sDItV8Rg296z3Khg3dcsvHp-va2y8lHj1OGEyDECG_w';
 
 export const publicRequest = axios.create( {
   baseURL: BASE_URL

c5ded6c688c533c2e5acb07d486098a205446834
Timothy Attah <timothyattah733@gmail.com>
2021-11-11 14:14:30 +0100

    added contents to widget small

--- admin/src/components/widgets/widgetLarge/WidgetLarge.jsx
+++ admin/src/components/widgets/widgetLarge/WidgetLarge.jsx
@@ -5,6 +5,7 @@ export const Container = styled.div`
 	flex: 2;
 	-webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
 	box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  padding: 20px;
 `;
 // export const Container = styled.div`
 
--- admin/src/components/widgets/widgetSmall/WidgetSmall.jsx
+++ admin/src/components/widgets/widgetSmall/WidgetSmall.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
+import { images } from '../../images';
+import { Visibility } from '@material-ui/icons';
 
 export const Container = styled.div`
 	flex: 1;
 	-webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
 	box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  padding: 20px;
+  margin-right: 20px;
+`;
+export const WidgetTitle = styled.span`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+export const WidgetList = styled.ul`
+
+`;
+export const WidgetListItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0;
+`;
+export const WidgetUser = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const WidgetUsername = styled.span`
+  font-weight: 600;
+`;
+export const WidgetUserTitle = styled.span`
+  font-weight: 300;
+`;
+export const WidgetButton = styled.button`
+	display: flex;
+	align-items: center;
+	border-radius: 10px;
+	padding: 7px 10px;
+	background-color: #eeeef7;
+	color: var(--text-gray);
+	.MuiSvgIcon-root {
+		margin-right: 5px;
+    font-size: 1rem;
+	}
 `;
 // export const Container = styled.div`
 
@@ -13,7 +53,42 @@ export const Container = styled.div`
 export const WidgetSmall = () => {
   return (
     <Container>
-      small
+      <WidgetTitle>New Join Members</WidgetTitle>
+      <WidgetList>
+        <WidgetListItem>
+          <Avatar><img src={images.ProPic} alt='' /></Avatar>
+          <WidgetUser>
+          <WidgetUsername>Anna Keller</WidgetUsername>
+          <WidgetUserTitle>Software Engineer</WidgetUserTitle>
+          </WidgetUser>
+          <WidgetButton>
+            <Visibility />
+            Display
+          </WidgetButton>
+        </WidgetListItem>
+        <WidgetListItem>
+          <Avatar><img src={images.ProPic} alt='' /></Avatar>
+          <WidgetUser>
+          <WidgetUsername>Anna Keller</WidgetUsername>
+          <WidgetUserTitle>Software Engineer</WidgetUserTitle>
+          </WidgetUser>
+          <WidgetButton>
+            <Visibility />
+            Display
+          </WidgetButton>
+        </WidgetListItem>
+        <WidgetListItem>
+          <Avatar><img src={images.ProPic} alt='' /></Avatar>
+          <WidgetUser>
+          <WidgetUsername>Anna Keller</WidgetUsername>
+          <WidgetUserTitle>Software Engineer</WidgetUserTitle>
+          </WidgetUser>
+          <WidgetButton>
+            <Visibility />
+            Display
+          </WidgetButton>
+        </WidgetListItem>
+      </WidgetList>
     </Container>
   )
 }

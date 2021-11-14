d38b9187f6beab85f940f94ca43241b5a707f8d8
Timothy Attah <timothyattah733@gmail.com>
2021-11-14 11:20:04 +0100

    Created and added userStats api call

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { Chart } from '../../components/chart/Chart';
 import { FeaturedInfo } from '../../components/featuredInfo/FeaturedInfo';
 import { userData } from '../../components/dummyData';
 import { WidgetLarge } from '../../components/widgets/widgetLarge/WidgetLarge';
 import { WidgetSmall } from '../../components/widgets/widgetSmall/WidgetSmall';
+import { userRequest } from '../../requestMethods';
 
 
 export const Container = styled.div`
@@ -19,10 +20,48 @@ export const HomeWidgets = styled.div`
 // `; 
 
 export const Home = () => {
+  const [ userStats, setUserStats ] = useState( [] );
+
+  const MONTHS = useMemo(
+    () => [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ], []
+  )
+
+  useEffect( () => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get( '/users/stats' )
+        res.data.map( item => {
+          return setUserStats(prev => [
+						...prev,
+						{ name: MONTHS[item._id - 1], 'Active User': item?.total },
+					]);
+        })
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    getStats()
+  }, [ MONTHS ] )
+  
+  console.log('userStats >>>> ',userStats);
+
   return (
     <Container>
       <FeaturedInfo />
-      <Chart data={ userData } title='User Analytics' grid activeData="Active user" />
+      <Chart data={ userStats } title='User Analytics' grid activeData="Active user" />
       <HomeWidgets>
         <WidgetSmall />
         <WidgetLarge />

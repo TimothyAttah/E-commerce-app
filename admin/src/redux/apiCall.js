5e301f76f4ce332cc65736a541a532c9e20b5fac
Timothy Attah <timothyattah733@gmail.com>
2021-11-15 13:38:32 +0100

    Created getProducts action call

--- admin/src/redux/apiCall.js
+++ admin/src/redux/apiCall.js
@@ -1,5 +1,6 @@
 import { loginStart, loginFailure, loginSuccess } from "./userSlice";
 import { publicRequest } from "../requestMethods";
+import { getProductStart, getProductSuccess } from "./productSlice";
 
 
 export const login = async ( dispatch, user ) => {
@@ -10,6 +11,18 @@ export const login = async ( dispatch, user ) => {
     localStorage.setItem( 'user', JSON.stringify(res.data));
     console.log( 'login', res.data );
    window.location('/')
+  } catch (err) {
+    dispatch(loginFailure())
+  }
+}
+
+export const getProducts = async ( dispatch ) => {
+  dispatch( getProductStart() );
+  try {
+    const res = await publicRequest.post( '/products' )
+    dispatch( getProductSuccess( res.data ) )
+    localStorage.setItem( 'user', JSON.stringify(res.data));
+    console.log( 'login', res.data );
   } catch (err) {
     dispatch(loginFailure())
   }

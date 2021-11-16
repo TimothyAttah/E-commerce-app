12acd1e53fa3c9e10a90f7cec14d71155ae64d21
Timothy Attah <timothyattah733@gmail.com>
2021-11-16 11:44:55 +0100

    Cread add file with firebse

--- admin/src/pages/products/ProductCreate.jsx
+++ admin/src/pages/products/ProductCreate.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage';
 import { app } from '../../firebase';
 
 export const Container = styled.div`
@@ -89,7 +89,37 @@ export const ProductCreate = () => {
 		e.preventDefault();
 		const fileName = new Date().getTime() + file.name;
 
-		const storage = getStorage(app)
+		const storage = getStorage( app )
+		const storageRef = ref( storage, fileName )
+		const uploadTask = uploadBytesResumable( storageRef, file )
+		
+		uploadTask.on(
+			'state_change',
+			( snapshot ) => {
+				const progress = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
+				console.log(`Upload is ${progress}% done`);
+				switch ( snapshot.state ) {
+					case 'paused':
+						console.log( 'Upload is paused' );
+						break
+					case 'running':
+						console.log( 'Upload is running' );
+						break
+					default:
+
+				}
+			},
+			( error ) => {
+				console.log(error);
+			},
+			() => {
+				getDownloadURL( uploadTask.snapshot.ref ).then( downloadURL => {
+					console.log( 'File available at ', downloadURL );
+					console.log({...inputs, });
+				})
+			}
+		)
+
 		const newProduct = {
 			file,
 			inputs,

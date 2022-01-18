7c6227c8734e38c3b53340b4a81a14ce3f629123
Timothy Attah <timothyattah733@gmail.com>
2022-01-18 11:15:53 +0100

    Change some data in dummy data file

--- admin/src/components/dummyData.js
+++ admin/src/components/dummyData.js
@@ -1,200 +1,212 @@
 import { images } from "./images";
 
-export const userData = [
-	{
-		name: 'Jan',
-		'Active user': 4000,
-	},
-	{
-		name: 'Feb',
-		'Active user': 3000,
-	},
-	{
-		name: 'Mar',
-		'Active user': 2245,
-	},
-	{
-		name: 'Apr',
-		'Active user': 1545,
-	},
-	{
-		name: 'May',
-		'Active user': 2160,
-	},
-	{
-		name: 'Jun',
-		'Active user': 3310,
-	},
-	{
-		name: 'Jul',
-		'Active user': 2330,
-	},
-	{
-		name: 'Aug',
-		'Active user': 2110,
-	},
-	{
-		name: 'Sep',
-		'Active user': 2340,
-	},
-	{
-		name: 'Oct',
-		'Active user': 2400,
-	},
-	{
-		name: 'Nov',
-		'Active user': 1500,
-	},
-	{
-		name: 'Dec',
-		'Active user': 1000,
-	},
+export const userData = [{
+        name: 'Jan',
+        'Active user': 4000,
+    },
+    {
+        name: 'Feb',
+        'Active user': 3000,
+    },
+    {
+        name: 'Mar',
+        'Active user': 2245,
+    },
+    {
+        name: 'Apr',
+        'Active user': 1545,
+    },
+    {
+        name: 'May',
+        'Active user': 2160,
+    },
+    {
+        name: 'Jun',
+        'Active user': 3310,
+    },
+    {
+        name: 'Jul',
+        'Active user': 2330,
+    },
+    {
+        name: 'Aug',
+        'Active user': 2110,
+    },
+    {
+        name: 'Sep',
+        'Active user': 2340,
+    },
+    {
+        name: 'Oct',
+        'Active user': 2400,
+    },
+    {
+        name: 'Nov',
+        'Active user': 1500,
+    },
+    {
+        name: 'Dec',
+        'Active user': 1000,
+    },
 ];
 
-export const productData = [
-	{
-		name: 'Jan',
-		'Sales': 4000
-	},
-	{
-		name: 'Feb',
-		'Sales': 3000
-	},
-	{
-		name: 'Mar',
-		'Sales': 5000
-	},
+export const productData = [{
+        name: 'Jan',
+        'Sales': 4500
+    },
+    {
+        name: 'Feb',
+        'Sales': 2000
+    },
+    {
+        name: 'Mar',
+        'Sales': 7000
+    },
 ]
 
-export const userRows = [
-	{
-		id: 1,
-		username: 'Jon Snow',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'jon@gmail.com',
-		status: 'active',
-		transaction: '$120.00',
-	},
-	{
-		id: 2,
-		username: 'Elizabeth Keen',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'elizabeth@gmail.com',
-		status: 'active',
-		transaction: '$190.00',
-	},
-	{
-		id: 3,
-		username: 'Matthew Peters',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'matthew@gmail.com',
-		status: 'active',
-		transaction: '$155.00',
-	},
-	{
-		id: 4,
-		username: 'Mark Leonard',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'mark@gmail.com',
-		status: 'active',
-		transaction: '$165.00',
-	},
-	{
-		id: 5,
-		username: 'Esther Susan',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'esther@gmail.com',
-		status: 'active',
-		transaction: '$150.00',
-	},
-	{
-		id: 6,
-		username: 'Bridget Starwood',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'bridget@gmail.com',
-		status: 'active',
-		transaction: '$125.00',
-	},
-	{
-		id: 7,
-		username: 'Bridget Starwood',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'bridget@gmail.com',
-		status: 'active',
-		transaction: '$125.00',
-	},
-	{
-		id: 8,
-		username: 'Bridget Starwood',
-		avatar: <img src={images.ProPic} alt='' />,
-		email: 'bridget@gmail.com',
-		status: 'active',
-		transaction: '$125.00',
-	},
+export const userRows = [{
+        id: 1,
+        username: 'Jon Snow',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'jon@gmail.com',
+        status: 'active',
+        transaction: '$120.00',
+    },
+    {
+        id: 2,
+        username: 'Elizabeth Keen',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'elizabeth@gmail.com',
+        status: 'active',
+        transaction: '$190.00',
+    },
+    {
+        id: 3,
+        username: 'Matthew Peters',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'matthew@gmail.com',
+        status: 'active',
+        transaction: '$155.00',
+    },
+    {
+        id: 4,
+        username: 'Mark Leonard',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'mark@gmail.com',
+        status: 'active',
+        transaction: '$160.00',
+    },
+    {
+        id: 5,
+        username: 'Esther Susan',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'esther@gmail.com',
+        status: 'active',
+        transaction: '$150.00',
+    },
+    {
+        id: 6,
+        username: 'Bridget Starwood',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'bridget@gmail.com',
+        status: 'active',
+        transaction: '$125.00',
+    },
+    {
+        id: 7,
+        username: 'Bridget Starwood',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'bridget@gmail.com',
+        status: 'active',
+        transaction: '$125.00',
+    },
+    {
+        id: 8,
+        username: 'Bridget Starwood',
+        avatar: < img src = { images.ProPic }
+        alt = '' / > ,
+        email: 'bridget@gmail.com',
+        status: 'active',
+        transaction: '$125.00',
+    },
 ];
-export const productRows = [
-	{
-		id: 1,
-		name: 'HP Laptop',
-		img: <img src={images.Img1} alt='' />,
-		stock: 123,
-		status: 'active',
-		price: '$120.00',
-	},
-	{
-		id: 2,
-		name: 'Apple IPhone',
-		img: <img src={images.Img2} alt='' />,
-		stock: 345,
-		status: 'active',
-		price: '$190.00',
-	},
-	{
-		id: 3,
-		name: 'Roller Skate',
-		img: <img src={images.Img3} alt='' />,
-		stock: 345,
-		status: 'active',
-		price: '$155.00',
-	},
-	{
-		id: 4,
-		name: 'Galaxy TV',
-		img: <img src={images.Img4} alt='' />,
-		stock: 'mark@gmail.com',
-		status: 'active',
-		price: '$165.00',
-	},
-	{
-		id: 5,
-		name: 'GH IPhone 7',
-		img: <img src={images.Img5} alt='' />,
-		stock: 567,
-		status: 'active',
-		price: '$150.00',
-	},
-	{
-		id: 6,
-		name: 'Robins Book',
-		img: <img src={images.Img6} alt='' />,
-		stock: 678,
-		status: 'active',
-		price: '$125.00',
-	},
-	{
-		id: 7,
-		name: 'Trousers Men',
-		img: <img src={images.Img7} alt='' />,
-		stock: 123,
-		status: 'active',
-		price: '$125.00',
-	},
-	{
-		id: 8,
-		name: 'Apple Airpods',
-		img: <img src={images.Img8} alt='' />,
-		stock: 789,
-		status: 'active',
-		price: '$125.00',
-	},
+export const productRows = [{
+        id: 1,
+        name: 'HP Laptop',
+        img: < img src = { images.Img1 }
+        alt = '' / > ,
+        stock: 123,
+        status: 'active',
+        price: '$120.00',
+    },
+    {
+        id: 2,
+        name: 'Apple IPhone',
+        img: < img src = { images.Img2 }
+        alt = '' / > ,
+        stock: 345,
+        status: 'active',
+        price: '$190.00',
+    },
+    {
+        id: 3,
+        name: 'Roller Skate',
+        img: < img src = { images.Img3 }
+        alt = '' / > ,
+        stock: 345,
+        status: 'active',
+        price: '$155.00',
+    },
+    {
+        id: 4,
+        name: 'Galaxy TV',
+        img: < img src = { images.Img4 }
+        alt = '' / > ,
+        stock: 'mark@gmail.com',
+        status: 'active',
+        price: '$165.00',
+    },
+    {
+        id: 5,
+        name: 'GH IPhone 7',
+        img: < img src = { images.Img5 }
+        alt = '' / > ,
+        stock: 567,
+        status: 'active',
+        price: '$150.00',
+    },
+    {
+        id: 6,
+        name: 'Robins Book',
+        img: < img src = { images.Img6 }
+        alt = '' / > ,
+        stock: 678,
+        status: 'active',
+        price: '$125.00',
+    },
+    {
+        id: 7,
+        name: 'Trousers Men',
+        img: < img src = { images.Img7 }
+        alt = '' / > ,
+        stock: 123,
+        status: 'active',
+        price: '$125.00',
+    },
+    {
+        id: 8,
+        name: 'Apple Airpods',
+        img: < img src = { images.Img8 }
+        alt = '' / > ,
+        stock: 789,
+        status: 'active',
+        price: '$125.00',
+    },
 ];
